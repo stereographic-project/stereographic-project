103d19ebd36d44b27264c92581ffcfe36350ca91
Ugo Brocard <ugo.brocard.official+github@gmail.com>
2023-12-04 18:46:53 +0100

    :sparkles: feature: add point_to_plane(...)

--- src/projection/stereographic.py
+++ src/projection/stereographic.py
@@ -1,22 +1,31 @@
-from coordinates import to_cartesian
-from geometry import Plane, Sphere, Line
-from geometry.plane import calculate_intersection
+from dataclasses import dataclass
 
+from geometry    import Plane, Sphere, Line
+from coordinates import Cartesian, Spherical, to_cartesian
+
+@dataclass
 class Stereographic:
-    def __init__(self, sphere: Sphere, plane: Plane) -> None:
-        self.sphere = sphere
-        self.plane  = plane
-
+    sphere: Sphere
+    plane:  Plane
+    
     def to_plane(self) -> Plane:
         for point in self.sphere.points:
-            cartesian = to_cartesian(point)
+            cartesian = self.point_to_plane(point)
             
-            if cartesian == self.sphere.pole:
+            if not cartesian:
                 continue
             
-            line = Line(self.sphere.pole, cartesian)
-            intersection = calculate_intersection(self.plane, line)
-            
-            self.plane + intersection
+            self.plane.add_point(cartesian)
             
         return self.plane
+    
+    def point_to_plane(self, point: Spherical) -> Cartesian:
+        cartesian = to_cartesian(point)
+            
+        if cartesian == self.sphere.pole:
+            return None
+        
+        line = Line(self.sphere.pole, cartesian)
+        intersection = self.plane.calculate_intersection(line)
+        
+        return intersection

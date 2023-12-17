5d75234c8eca869cd59fdd74e254355a5532ccbf
Ugo Brocard <137636358+ugo-brocard@users.noreply.github.com>
2023-12-17 21:06:49 +0100

    :sparkles: feature: starting the development of a custom renderer with pygame

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
-from matrices import Matrix
+from rendering import Renderer
+from coordinates import Spherical
 
-print(Matrix([[10, 10, 1], [10, 1, 10], [1, 10, 10]])@Matrix([[10], [12], [13]]))
+from geometry import Sphere, Plane
+from projection import Stereographic
+from rotations import Rotation
+
+import random
+
+points = []
+for _ in range(20000):
+    points.append(Spherical(10, 30, random.uniform(0, 360)))    
+
+sphere = Sphere(10, points)
+sphere.rotate(Rotation(20, 50, 0))
+plane = Plane(-30)
+
+stereographic = Stereographic(sphere, plane)
+points = stereographic.to_plane().points
+
+Renderer().run(points)
